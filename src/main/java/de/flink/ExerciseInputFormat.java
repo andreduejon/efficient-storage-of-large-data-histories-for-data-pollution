537f3a2baad9597d0ace63123b30133583cc03c0
andre.duejon <a.dujeon@outlook.com>
2020-08-09 17:30:52 +0200

    hbase (1)

--- src/main/java/de/flink/ExerciseInputFormat.java
+++ src/main/java/de/flink/ExerciseInputFormat.java
@@ -6,10 +6,16 @@ import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.io.InputSplitAssigner;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.*;
 import java.lang.reflect.Field;
-import java.sql.*;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -27,35 +33,35 @@ public class ExerciseInputFormat implements InputFormat<HistoryObjects, SheetInp
     private Iterator<HistoryObjects> historyIterator;
     private SheetInputSplit1 sheetInputSplit1;
     private String[] dates = {
-            "2008-11-04",
-            "2009-01-01",
-            "2014-01-01", "2014-01-01",
-            "2014-05-06", "2014-05-06",
-            "2016-01-01", "2016-01-01", "2016-01-01",
-            "2016-03-15", "2016-03-15", "2016-03-15",
-            "2019-01-01", "2019-01-01", "2019-01-01", "2019-01-01",
-            "2011-01-01",
-            "2010-01-01",
-            "2014-07-15", "2014-07-15",
-            "2014-11-04", "2014-11-04",
-            "2017-09-12", "2017-09-12", "2017-09-12", "2017-09-12",
-            "2017-10-10", "2017-10-10", "2017-10-10", "2017-10-10",
-            "2018-01-01", "2018-01-01", "2018-01-01", "2018-01-01",
-            "2012-07-17", "2012-07-17",
-            "2012-11-06", "2012-11-06",
-            "2013-01-01", "2013-01-01",
-            "2015-01-01", "2015-01-01", "2015-01-01",
-            "2015-09-15", "2015-09-15", "2015-09-15",
-            "2015-10-06", "2015-10-06", "2015-10-06",
-            "2015-11-03", "2015-11-03", "2015-11-03",
-            "2018-05-08", "2018-05-08", "2018-05-08", "2018-05-08",
-            "2018-11-06", "2018-11-06", "2018-11-06", "2018-11-06",
-            "2012-01-01",
-            "2012-05-08",
-            "2016-06-07", "2016-06-07", "2016-06-07",
-            "2016-11-08", "2016-11-08", "2016-11-08",
-            "2017-01-01", "2017-01-01", "2017-01-01", "2017-01-01",
-            "2017-11-07", "2017-11-07", "2017-11-07", "2017-11-07"
+            "20081104",
+            "20090101",
+            "20140101", "20140101",
+            "20140506", "20140506",
+            "20160101", "20160101", "20160101",
+            "20160315", "20160315", "20160315",
+            "20190101", "20190101", "20190101", "20190101",
+            "20110101",
+            "20100101",
+            "20140715", "20140715",
+            "20141104", "20141104",
+            "20170912", "20170912", "20170912", "20170912",
+            "20171010", "20171010", "20171010", "20171010",
+            "20180101", "20180101", "20180101", "20180101",
+            "20120717", "20120717",
+            "20121106", "20121106",
+            "20130101", "20130101",
+            "20150101", "20150101", "20150101",
+            "20150915", "20150915", "20150915",
+            "20151006", "20151006", "20151006",
+            "20151103", "20151103", "20151103",
+            "20180508", "20180508", "20180508", "20180508",
+            "20181106", "20181106", "20181106", "20181106",
+            "20120101",
+            "20120508",
+            "20160607", "20160607", "20160607",
+            "20161108", "20161108", "20161108",
+            "20170101", "20170101", "20170101", "20170101",
+            "20171107", "20171107", "20171107", "20171107"
     };
     private String[] attributes = {
             "house_num", "street_name", "zip_code", "phone_num", "age_group",
@@ -107,112 +113,121 @@ public class ExerciseInputFormat implements InputFormat<HistoryObjects, SheetInp
         this.sheetInputSplit1 = sheetInputSplit1;
         String[] ncidBatch = sheetInputSplit1.getNcid1().split("#");
         try {
-            Class.forName("org.postgresql.Driver");
-            Connection c = null;
-            Statement stmt = null;
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
-                            "postgres", "postgres");
-            if (ncidBatch.length > 0) {
-                // Variables to store a value which detemrines the starting point in tghe attributes and dates array.
-                int startAtAttribute = 0;
-                int startAtDate = 0;
+            org.apache.hadoop.conf.Configuration config = HBaseConfiguration.create();
 
-                // Iterate over all ncids in batch
-                for (String ncid : ncidBatch) {
-                    ReplacementEntry initialEntry = null;
-                    stmt = c.createStatement();
-                    String sql = "SELECT * FROM replacement WHERE nc_id='" + ncid + "' AND timestamp <= '2020-01-01' ORDER BY timestamp DESC LIMIT 1;";
-                    ResultSet rs = stmt.executeQuery(sql);
-                    while (rs.next()) {
-                        initialEntry = new ReplacementEntry(
-                                rs.getInt("id"),
-                                rs.getString("nc_id"),
-                                rs.getTimestamp("timestamp").toLocalDateTime(),
-                                rs.getString("county_id"),
-                                rs.getString("county_desc"),
-                                rs.getString("last_name"),
-                                rs.getString("first_name"),
-                                rs.getString("midl_name"),
-                                rs.getString("house_num"),
-                                rs.getString("street_dir"),
-                                rs.getString("street_name"),
-                                rs.getString("res_city_desc"),
-                                rs.getString("state_cd"),
-                                rs.getString("zip_code"),
-                                rs.getString("area_cd"),
-                                rs.getString("phone_num"),
-                                rs.getString("race_code"),
-                                rs.getString("race_desc"),
-                                rs.getString("ethnic_code"),
-                                rs.getString("ethnic_desc"),
-                                rs.getString("party_cd"),
-                                rs.getString("party_desc"),
-                                rs.getString("sex_code"),
-                                rs.getString("sex"),
-                                rs.getString("age"),
-                                rs.getString("birth_place"),
-                                rs.getString("age_group"),
-                                rs.getString("name_prefx_cd"),
-                                rs.getString("name_sufx_cd"),
-                                rs.getString("half_code"),
-                                rs.getString("street_type_cd"),
-                                rs.getString("street_sufx_cd"),
-                                rs.getString("unit_designator"),
-                                rs.getString("unit_num"),
-                                rs.getString("mail_addr1"),
-                                rs.getString("mail_addr2"),
-                                rs.getString("mail_addr3"),
-                                rs.getString("mail_addr4"),
-                                rs.getString("mail_city"),
-                                rs.getString("mail_state"),
-                                rs.getString("mail_zipcode"));
-                    }
-                    if(initialEntry != null) {
-                        for(int i = 0; i < Long.parseLong(this.outdatedFrequency); i++) {
-                            String date = dates[startAtDate];
-                            String attribute = attributes[startAtAttribute];
-                            Field preField = ReplacementEntry.class.getField(attribute);
-                            String preValue = (String) preField.get(initialEntry);
+            try {
+                org.apache.hadoop.hbase.client.Connection connection = ConnectionFactory.createConnection(config);
+                Table table = connection.getTable(TableName.valueOf("history"));
 
-                            sql = "SELECT " + attribute +" FROM replacement WHERE nc_id='" + ncid + "' AND timestamp <= '"+ date +"' ORDER BY timestamp DESC LIMIT 1;";
-                            rs = stmt.executeQuery(sql);
-                            if(!rs.isBeforeFirst()) {
-                                sql = "SELECT " + attribute +" FROM replacement WHERE nc_id='" + ncid + "' AND timestamp >= '"+ date +"' ORDER BY timestamp ASC LIMIT 1;";
+                if (ncidBatch.length > 0) {
+                    // Variables to store a value which detemrines the starting point in tghe attributes and dates array.
+                    int startAtAttribute = 0;
+                    int startAtDate = 0;
+
+                    // Iterate over all ncids in batch
+                    for (String ncid : ncidBatch) {
+                        ReplacementEntry initialEntry = null;
+
+                        Get get = new Get(Bytes.toBytes(ncid));
+                        get.addFamily(Bytes.toBytes("data"));
+
+                        Result rs = table.get(get);
+                        if (rs.getExists()) {
+                            initialEntry = new ReplacementEntry(
+                                    0,
+                                    ncid,
+                                    null,
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("county_id"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("county_desc"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("last_name"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("first_name"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("midl_name"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("house_num"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("street_dir"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("street_name"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("res_city_desc"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("state_cd"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("zip_code"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("area_cd"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("phone_num"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("race_code"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("race_desc"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("ethnic_code"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("ethnic_desc"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("party_cd"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("party_desc"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("sex_code"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("sex"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("age"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("birth_place"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("age_group"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("name_prefx_cd"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("name_sufx_cd"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("half_code"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("street_type_cd"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("street_sufx_cd"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("unit_designator"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("unit_num"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("mail_addr1"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("mail_addr2"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("mail_addr3"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("mail_addr4"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("mail_city"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("mail_state"))),
+                                    Bytes.toString(rs.getValue(Bytes.toBytes("data"),Bytes.toBytes("mail_zipcode"))));
+
+                            for(int i = 0; i < Long.parseLong(this.outdatedFrequency); i++) {
+                                String date = dates[startAtDate];
+                                String attribute = attributes[startAtAttribute];
+                                Field preField = ReplacementEntry.class.getField(attribute);
+                                String preValue = (String) preField.get(initialEntry);
+
+                                sql = "SELECT " + attribute +" FROM replacement WHERE nc_id='" + ncid + "' AND timestamp <= '"+ date +"' ORDER BY timestamp DESC LIMIT 1;";
                                 rs = stmt.executeQuery(sql);
+
+                                if(!rs.isBeforeFirst()) {
+                                    sql = "SELECT " + attribute +" FROM replacement WHERE nc_id='" + ncid + "' AND timestamp >= '"+ date +"' ORDER BY timestamp ASC LIMIT 1;";
+                                    rs = stmt.executeQuery(sql);
+                                }
+
+                                String newValue = null;
+                                while(rs.next()) {
+                                    Field field = ReplacementEntry.class.getField(attribute);
+                                    field.set(initialEntry, rs.getString(1));
+                                    newValue = rs.getString(1);
+                                }
+                                if(preValue != null && !preValue.equals(newValue)){
+                                    System.out.println("#" + (i+1) + "---NCID: " + ncid + "---Date: " + date + "---Attribute: " + attribute + "---Old: " + preValue  + "---New: " + newValue);
+                                    //System.out.println("#" + (i+1) + "---SQL: " + sql);
+                                }
+
+                                if(startAtAttribute < attributes.length -1) {
+                                    startAtAttribute++;
+                                } else {
+                                    startAtAttribute = 0;
+                                }
+                                if(startAtDate < dates.length -1) {
+                                    startAtDate++;
+                                } else {
+                                    startAtDate = 0;
+                                }
                             }
 
-                            String newValue = null;
-                            while(rs.next()) {
-                                Field field = ReplacementEntry.class.getField(attribute);
-                                field.set(initialEntry, rs.getString(1));
-                                newValue = rs.getString(1);
-                            }
-                            if(preValue != null && !preValue.equals(newValue)){
-                                System.out.println("#" + (i+1) + "---NCID: " + ncid + "---Date: " + date + "---Attribute: " + attribute + "---Old: " + preValue  + "---New: " + newValue);
-                                //System.out.println("#" + (i+1) + "---SQL: " + sql);
-                            }
-
-                            if(startAtAttribute < attributes.length -1) {
-                                startAtAttribute++;
-                            } else {
-                                startAtAttribute = 0;
-                            }
-                            if(startAtDate < dates.length -1) {
-                                startAtDate++;
-                            } else {
-                                startAtDate = 0;
-                            }
+                            // Time-out the process to inject further errors in addition to outdated values. This should account
+                            // for the fact that histories of different size take a different amount of time to process.
+                            long timeout = (Integer.parseInt(this.outdatedFrequency) * Integer.parseInt(this.processingTime));
+                            TimeUnit.MILLISECONDS.sleep(timeout);
                         }
                     }
+                    table.close();
+                    connection.close();
 
-                    // Time-out the process to inject further errors in addition to outdated values. This should account
-                    // for the fact that histories of different size take a different amount of time to process.
-                    long timeout = (Integer.parseInt(this.outdatedFrequency) * Integer.parseInt(this.processingTime));
-                    TimeUnit.MILLISECONDS.sleep(timeout);
-                }
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            c.close();
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
